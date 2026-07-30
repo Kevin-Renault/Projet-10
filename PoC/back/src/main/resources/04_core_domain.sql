73c1e0f1d160965bbba5bfe366a0ba67953d99ae
Kevin Renault <kevin.renault.92@gmail.com>
2026-07-30 23:04:06 +0200

    fix: restore omitted database schema fields

--- PoC/back/src/main/resources/04_core_domain.sql
+++ PoC/back/src/main/resources/04_core_domain.sql
@@ -22,6 +22,7 @@ DROP TABLE IF EXISTS currency CASCADE;
 -- ////////////////////////////////////////////////////////////////////////////
 CREATE TABLE IF NOT EXISTS vehicle (
     id BIGSERIAL PRIMARY KEY,
+    vehicle_uuid uuid NOT NULL DEFAULT gen_random_uuid(),
     reference VARCHAR(100) NULL,
     make VARCHAR(100) NULL,
     model VARCHAR(100) NULL,
